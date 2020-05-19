7605805eaeea232105e3b0e3377fa8c9a4419e6d
Mikhail Rusakovich <rusakovichma@gmail.com>
2020-05-20 00:05:49 +0300

    spidering logic modify

--- src/test/java/com/github/rusakovichma/dvwa/bdd/attacks/SpideringAttack.java
+++ src/test/java/com/github/rusakovichma/dvwa/bdd/attacks/SpideringAttack.java
@@ -13,15 +13,17 @@ import static junit.framework.TestCase.assertTrue;
 public class SpideringAttack {
 
     private DastTool dastTool;
+    private String target;
 
     @Given("^perform spidering with 'Zap' DAST tool on the main page: '(.*)'$")
     public void perform_spidering_with_Zap_DAST_tool_on_the_main_page(String target) throws Throwable {
         this.dastTool = new ZapDastTool(ZAP_ADDRESS, ZAP_PORT, ZAP_API_KEY, target);
+        this.target = target;
 
         dastTool.doSpidering();
     }
 
-    @Then("^there is no any unrestricted URLs, functionality, Medium or High security issues found$")
+    @Then("^there is no any unrestricted URLs, functionality or High security issues found$")
     public void no_any_unrestricted_URLs_functionality_or_security_issues_found() throws Throwable {
         Thread.sleep(10000l);
         DastReport report = dastTool.getReport();
@@ -31,9 +33,12 @@ public class SpideringAttack {
             for (final DastReport.Site.Alert alert: site.getAlerts()){
                 //Verify High level alerts
                 assertTrue(alert.getRiskcode() < 3);
+
+                //Verify to view the directory listing
+                if (alert.getAlert().equals("Directory Browsing") && alert.getConfidence() >=2 ){
+                    assertTrue(alert.getInstances().isEmpty());
+                }
             }
-            //Verify SSL for site
-            assertTrue(site.isSsl());
         }
     }
 
